60bb2be983ed697bcf4a611abc7a3542d8ccb021
ARHITEKTOR <danilov@mail.de>
2023-11-08 14:55:57 +0100

    Update DNS IPv4 ändern jetzt möglich

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import wmi
 import logging
+import info_ausgabe
+
 
 # Konfigurieren Sie das Logging
 logging.basicConfig(filename='dns_settings.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
 
 
-def change_dns_settings(dns_server_ipv4, dns_server_ipv6):
+def change_dns_settings(primary_dns_ipv4, secondary_dns_ipv4):
     try:
         # Initialisieren Sie das WMI-Objekt
+        logging.info("Initialisiere das WMI-Objekt")
         wmi_obj = wmi.WMI()
 
         # Ermitteln Sie die Netzwerkschnittstellen
+        logging.info("Ermittle die Netzwerkschnittstellen")
         network_interfaces = wmi_obj.Win32_NetworkAdapterConfiguration()
 
         for interface in network_interfaces:
             # Überprüfen Sie, ob die Netzwerkschnittstelle IPv4-Unterstützung hat
-            if interface.IPEnabled:
-                # Ändern Sie die IPv4-DNS-Server
-                if dns_server_ipv4:
-                    logging.info("Ändere den IPv4-DNS-Server auf %s für die Schnittstelle %s", dns_server_ipv4, interface.Description)
-                    interface.SetDNSServerSearchOrder(DNSServerSearchOrder=[dns_server_ipv4])
-
-            # Überprüfen Sie, ob IPv6-Unterstützung für diese Schnittstelle verfügbar ist
-            if hasattr(interface, "IPv6Enabled") and interface.IPv6Enabled:
-                # Ändern Sie die IPv6-DNS-Server
-                if dns_server_ipv6:
-                    logging.info("Ändere den IPv6-DNS-Server auf %s für die Schnittelle %s", dns_server_ipv6, interface.Description)
-                    interface.SetDNSServerSearchOrder(DNSServerSearchOrder=[dns_server_ipv6])
+            logging.info("Überprüfe IPv4-Unterstützung für die Schnittstelle %s", interface.Description)
+            # Ändern Sie die IPv4-DNS-Server
+            dns_servers = [server for server in [primary_dns_ipv4, secondary_dns_ipv4] if server]
+            logging.info("Ändere die IPv4-DNS-Server auf %s für die Schnittelle %s", dns_servers, interface.Description)
+            interface.SetDNSServerSearchOrder(DNSServerSearchOrder=dns_servers)
 
         logging.info("DNS-Einstellungen erfolgreich geändert.")
     except Exception as e:
@@ -34,8 +31,17 @@ def change_dns_settings(dns_server_ipv4, dns_server_ipv6):
 
 
 # Konfigurieren Sie die gewünschten DNS-Server
-dns_server_ipv4 = "8.8.8.8"
-dns_server_ipv6 = "2001:4860:4860::8888"
+primary_dns_ipv4 = "94.140.14.49"
+secondary_dns_ipv4 = "94.140.14.59"
+
+print(10 * "_", "Daten vorher:", 10 * "_")
+info_ausgabe.print_network_settings()
+print(50 * "-")
 
 # Rufen Sie die Funktion zum Ändern der DNS-Einstellungen auf
-change_dns_settings(dns_server_ipv4, dns_server_ipv6)
+logging.info("Rufe die Funktion zum Ändern der DNS-Einstellungen auf")
+change_dns_settings(primary_dns_ipv4, secondary_dns_ipv4)
+
+print(10 * "_", "Daten vorher:", 10 * "_")
+info_ausgabe.print_network_settings()
+print(50 * "-")
