5d84e7d7034695b6533c742aefd1ff898194dd26
ARHITEKTOR <danilov@mail.de>
2023-11-08 18:23:24 +0100

    Update DNS IPv4 ändern jetzt möglich

--- ausgabe.py
+++ ausgabe.py
@@ -1,13 +1,21 @@
+gesamt_breite = 130
+text_lange = 0
+
+
 def ausgabe(text):
-    print("\n")
-    zeichen_anzahl = len(text)
-    zeichen_anzahl = zeichen_anzahl + 44
-    print(20*"_" + "[", text, "]" + 20 * "_")
+    global text_lange
+    text = str(text)
+    text = text.upper()
+    text_lange = len(text)
+    zeichen_anzahl = (gesamt_breite - text_lange) / 2
+    zeichen_anzahl = int(zeichen_anzahl)
+    print(zeichen_anzahl * "_" + "[ " + text + " ]" + zeichen_anzahl * "_")
     return zeichen_anzahl
 
 
 def unterstrich(zeichen_anzahl):
-    print(zeichen_anzahl * "_")
+    anzahl_zeichen_gesamt = zeichen_anzahl * 2 + text_lange + 4
+    print(anzahl_zeichen_gesamt * "-")
     print("\n")
 
 
--- main.py
+++ main.py
@@ -1,24 +1,37 @@
+import sys
+
 import ausgabe
 import ipv4_change
 import einstellungen_anzeigen
 
+try:
+    # Konfigurieren Sie die gewünschten DNS-Server
+    primary_dns_ipv4 = "94.140.14.49"
+    secondary_dns_ipv4 = "94.140.14.59"
 
-# Konfigurieren Sie die gewünschten DNS-Server
-primary_dns_ipv4 = "94.140.14.49"
-secondary_dns_ipv4 = "94.140.14.59"
+    # Log einträge leeren
+    with open("dns_settings.log", "r+") as f:
+        f.truncate(0)
 
-# Log einträge leeren
-with open("dns_settings.log", "r+") as f:
-    f.truncate(0)
+    zeichenanzahl = ausgabe.ausgabe("Aktuelle Einstellungen")
+    einstellungen_anzeigen.print_network_settings()
+    ausgabe.unterstrich(zeichenanzahl)
+
+    zeichenanzahl = ausgabe.ausgabe("Diese Einstellungen werden geändert")
+    print("DNS-Server IPv4 Primär: ", primary_dns_ipv4)
+    print("DNS-Server IPv4 Alternative: ", secondary_dns_ipv4)
+    ausgabe.unterstrich(zeichenanzahl)
+
+    input("Drücken sie Enter um die Einstellungen auf folgende daten zu setzen:")
+
+    ipv4_change.change_dns_settings(primary_dns_ipv4, secondary_dns_ipv4)
+
+    zeichenanzahl = ausgabe.ausgabe("Einstellungen nach der Änderung")
+    einstellungen_anzeigen.print_network_settings()
+    ausgabe.unterstrich(zeichenanzahl)
+    input("Zum beenden [Enter] drücken:")
 
 
-zeichenanzahl = ausgabe.ausgabe("Aktuelle Einstellungen")
-einstellungen_anzeigen.print_network_settings()
-ausgabe.unterstrich(zeichenanzahl)
-
-
-ipv4_change.change_dns_settings(primary_dns_ipv4, secondary_dns_ipv4)
-
-zeichenanzahl = ausgabe.ausgabe("Aktuelle Einstellungen")
-einstellungen_anzeigen.print_network_settings()
-ausgabe.unterstrich(zeichenanzahl)
+except KeyboardInterrupt:
+    print("\n")
+    sys.exit("Beendet durch Benutzer.")
