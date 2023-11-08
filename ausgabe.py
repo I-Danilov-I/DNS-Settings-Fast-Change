1ccd4615aae30430015daa4ce945daadc7ef65cd
ARHITEKTOR <danilov@mail.de>
2023-11-08 18:45:32 +0100

    Update DNS IPv4 ändern jetzt möglich

--- ausgabe.py
+++ ausgabe.py
@@ -9,6 +9,7 @@ def ausgabe(text):
     text_lange = len(text)
     zeichen_anzahl = (gesamt_breite - text_lange) / 2
     zeichen_anzahl = int(zeichen_anzahl)
+    print("\n")
     print(zeichen_anzahl * "_" + "[ " + text + " ]" + zeichen_anzahl * "_")
     return zeichen_anzahl
 
@@ -16,7 +17,6 @@ def ausgabe(text):
 def unterstrich(zeichen_anzahl):
     anzahl_zeichen_gesamt = zeichen_anzahl * 2 + text_lange + 4
     print(anzahl_zeichen_gesamt * "-")
-    print("\n")
 
 
 def ausgabe_log(text):
